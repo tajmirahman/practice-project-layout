3d2877a17be3c1583a3afc534394cf8b16ddcb08
Tajmilur Rahman <tajmilurrahman91@gmail.com>
2025-12-23 18:10:02 +0600

    categoryNews page working

--- my-project/src/Pages/CategoryNews.jsx
+++ my-project/src/Pages/CategoryNews.jsx
@@ -1,17 +1,30 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import { useLoaderData, useParams } from 'react-router-dom';
 import NewsCard from './NewsCard';
 
 const CategoryNews = () => {
     const data= useLoaderData();
     const {id}= useParams();
-    console.log(data,id)
+    // console.log(data,id)
+    const [categoryNews,setCategoryNews]=useState([]);
+
+    useEffect(()=>{
+
+        const newsFilter= data.filter(singleNews=> singleNews.category_id == id);
+        setCategoryNews(newsFilter);
+
+        
+    },[data, id])
+
+
+ 
 
 
     return (
         <div>
+            
             {
-                data.map(news=> <NewsCard news={news}></NewsCard>)
+                categoryNews.map(news=> <NewsCard news={news}></NewsCard>)
             }
         </div>
     );

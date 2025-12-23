eb2c7d64fdc4e41569a31b8c1246420f123b5a3b
Tajmilur Rahman <tajmilurrahman91@gmail.com>
2025-12-23 18:32:59 +0600

    data category show done ui

--- my-project/src/Pages/CategoryNews.jsx
+++ my-project/src/Pages/CategoryNews.jsx
@@ -3,28 +3,38 @@ import { useLoaderData, useParams } from 'react-router-dom';
 import NewsCard from './NewsCard';
 
 const CategoryNews = () => {
-    const data= useLoaderData();
-    const {id}= useParams();
-    // console.log(data,id)
-    const [categoryNews,setCategoryNews]=useState([]);
+    const data = useLoaderData();
+    const { id } = useParams();
+    console.log(data, id)
+    const [categoryNews, setCategoryNews] = useState([]);
 
-    useEffect(()=>{
+    useEffect(() => {
 
-        const newsFilter= data.filter(singleNews=> singleNews.category_id == id);
-        setCategoryNews(newsFilter);
+        if (id == '0') {
+            setCategoryNews(data);
+            return;
+        } else if (id == '1') {
+            const findNews = data.filter(news => news.others.is_today_pick == true);
+            setCategoryNews(findNews);
+        } else {
+            const newsFilter = data.filter(singleNews => singleNews.category_id == id);
+            setCategoryNews(newsFilter);
+        }
 
-        
-    },[data, id])
 
 
- 
+
+    }, [data, id])
+
+
+
 
 
     return (
         <div>
-            
+
             {
-                categoryNews.map(news=> <NewsCard news={news}></NewsCard>)
+                categoryNews.map(news => <NewsCard news={news}></NewsCard>)
             }
         </div>
     );
